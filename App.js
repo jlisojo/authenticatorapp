ffec3f6b561920a736605a8841cc1eaeadbc25c2
Joshua Lisojo <jlisojo@gmail.com>
2020-07-07 13:16:54 -0400

    added cool

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // Testing git
 // Josh Lisojo
-
+// Cool! :)
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
